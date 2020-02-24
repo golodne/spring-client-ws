78a64fa7d436fa69487d200770c9a92773c2205c
vgoncharov <goncharov.saratov@gmail.com>
2020-02-24 22:04:57 +0300

    init client project and config gradle.build

---
+++ src/test/java/ru/consumer/springclientws/SpringClientWsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.consumer.springclientws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringClientWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
